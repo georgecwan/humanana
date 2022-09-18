ffc1b004031f8ae71aa23e0ee4573eaedde184d1
georgecwan <georgecwan@users.noreply.github.com>
2022-09-18 05:02:58 -0400

    Rename MLModel

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ print(predictions)
 image_input = ct.ImageType(shape=(1, 256, 256, 1), color_layout='G')
 classifier_config = ct.ClassifierConfig(class_labels=class_labels)
 mlmodel = ct.convert(model, source="tensorflow", inputs=[image_input], classifier_config=classifier_config)
-mlmodel.save('model.mlmodel')
+mlmodel.save('bananaModel.mlmodel')
 
 exImage = Image.open(FILE_NAME).resize((256, 256))
 exImage = ImageOps.grayscale(exImage)
--- modeltrainer.py
+++ modeltrainer.py
@@ -46,11 +46,11 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-normalization_layer = layers.Rescaling(1./255)
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-print(np.min(first_image), np.max(first_image))
+# normalization_layer = layers.Rescaling(1./255)
+# normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# image_batch, labels_batch = next(iter(normalized_ds))
+# first_image = image_batch[0]
+# print(np.min(first_image), np.max(first_image))
 
 num_classes = len(class_names)
 
