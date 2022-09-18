791ef38662c093512efc50589c885a941fc28e0e
georgecwan <georgecwan@users.noreply.github.com>
2022-09-18 03:33:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import coremltools as ct
-from PIL import Image
-image_input = ct.ImageType(shape=(1, 256, 256, 1), scale=1/255)
+from PIL import Image, ImageOps
 
-classifier_config = ct.ClassifierConfig(class_labels=['banana', 'human'])
+image_input = ct.ImageType(shape=(1, 256, 256, 1), scale=1/255, bias=0, color_layout='G')
+classifier_config = ct.ClassifierConfig(class_labels=["banana", "human"])
+mlmodel = ct.convert('model', source="tensorflow", inputs=[image_input], classifier_config=classifier_config)
 
-mlmodel = ct.convert('model.mlmodel', source="tensorflow", inputs=[image_input], classifier_config=classifier_config)
-# mlmodel.save('banana.mlmodel')
-print(mlmodel.get_spec().description)
-exim = Image.open('../images/IMG_6639.jpeg').resize((256, 256))
-out_dict = mlmodel.predict({"rescaling_1_input": exim})
+exImage = Image.open('274234352_368217161786364_4653297888454117985_n.jpg').resize((256, 256))
+exImage = ImageOps.grayscale(exImage)
+out_dict = mlmodel.predict({'rescaling_1_input': exImage})
 print(out_dict)
