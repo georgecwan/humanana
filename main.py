f214f2c1a914e9787da10bb23006e7fbf79aa251
georgecwan <georgecwan@users.noreply.github.com>
2022-09-18 04:01:28 -0400

    Clean up .idea folder

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import coremltools as ct
 from PIL import Image, ImageOps
 
-image_input = ct.ImageType(shape=(1, 256, 256, 1), scale=1/255, bias=0, color_layout='G')
+image_input = ct.ImageType(shape=(1, 256, 256, 1), scale=1/255, color_layout='G')
 classifier_config = ct.ClassifierConfig(class_labels=["banana", "human"])
 mlmodel = ct.convert('model', source="tensorflow", inputs=[image_input], classifier_config=classifier_config)
 
-exImage = Image.open('274234352_368217161786364_4653297888454117985_n.jpg').resize((256, 256))
+exImage = Image.open('banana_stock.jpg').resize((256, 256))
 exImage = ImageOps.grayscale(exImage)
 out_dict = mlmodel.predict({'rescaling_1_input': exImage})
 print(out_dict)
